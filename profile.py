895b3726de7ab53e7e1212359f6ce787fdcfdf25
Jessica Lizet Pablo <jessica@Jessicas-MacBook-Pro.local>
2026-06-16 14:09:31 -0700

    Add Python profile variables lab

---
+++ profile.py
@@ -0,0 +1,15 @@
+full_name = "Jessica Lizet Pablo"
+print(full_name)
+nickname = "Jess"
+print(nickname)
+age = 17
+print(age)
+used_python_before = False
+hobbies = ["Music", "Watching_shows", "Baking"]
+print(hobbies)
+favorite ={
+    "movie": "Insidious",
+    "hobby": "Baking",
+    "food": "Burritos"
+}
+print(favorite)
